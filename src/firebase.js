6f1823ff4fec5289d68e32a871eca319e6f8ffdf
ben mckenna <benmckenna2000@outlook.com>
2020-12-23 01:52:37 +0000

    header made

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA1rMyHexpB91lpQmpHZJUFuGZbgGhRG0c",
+  authDomain: "clone-47a9d.firebaseapp.com",
+  projectId: "clone-47a9d",
+  storageBucket: "clone-47a9d.appspot.com",
+  messagingSenderId: "242569356808",
+  appId: "1:242569356808:web:bde96d9843861195da13bd",
+  measurementId: "G-2BQXSBZWWT",
+};
